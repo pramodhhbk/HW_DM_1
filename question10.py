0e01a1bede3f53a973c4cbac093b62473b7a0a56
Pramodh Sairam <45509400+pramodhhbk@users.noreply.github.com>
2024-01-30 23:24:40 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -22,11 +22,11 @@ def question10_5():
     return answer
 
 def question10_6():
-    answer = True
+    answer = False
     return answer
 
 def question10_7():
-    answer = True
+    answer = False
     return answer
 
 def question10_8():
