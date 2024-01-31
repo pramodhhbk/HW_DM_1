db9e683717c2786b59156b8e1890da58531a8d6c
Pramodh Sairam <45509400+pramodhhbk@users.noreply.github.com>
2024-01-30 23:05:56 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -6,14 +6,14 @@ def question6_1():
     }
     """
     answer = {
-        'equal_width': ['No change', 9],
-        'equal_frequency': ['No change', 9]
+        'equal_width': ['No change', 3],
+        'equal_frequency': ['No change', 3]
     }
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['No change', 9],
+        'equal_width': ['No change', 7],
         'equal_frequency': ['No change', 9]
     }
     return answer
